ca02ec26e017fc2fbd829715e703228736fe690d
Md Rana Mahmud <ranamd0@gmail.com>
2018-01-10 23:00:21 +0600

    Order email intent added

--- app/src/main/java/com/ranamahmud/justjava/MainActivity.java
+++ app/src/main/java/com/ranamahmud/justjava/MainActivity.java
@@ -1,11 +1,13 @@
 package com.ranamahmud.justjava;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -20,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText editTextName = findViewById(R.id.editTextName);
-        name = String.valueOf(editTextName.getText());
+
+
 
 
 
@@ -32,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = findViewById(R.id.order_summary_text_view);
         priceTextView.setText(createOrderSummary(quantity));
       //  priceTextView.setText("Total price: "+NumberFormat.getCurrencyInstance().format(quantity*5)+"\nThank you!");
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Order Coffee");
+        intent.putExtra(Intent.EXTRA_STREAM, createOrderSummary(quantity));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
 
     }
 //displays value
@@ -51,6 +61,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void increment(View view) {
 
+if(quantity==100)
+{
+    Toast.makeText(this, "You can't have more than 100 cup coffee.", Toast.LENGTH_SHORT).show();
+    return;
+}
         quantity += 1;
         displayQuantity(quantity);
         displayPrice(calculatePrice(quantity));
@@ -58,7 +73,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void decrement(View view) {
-        if(quantity>0)
+        if(quantity==1){
+            Toast.makeText(this, "You can't have less than 1 cup of coffee.", Toast.LENGTH_SHORT).show();
+            return;
+        }
             quantity -= 1;
         displayQuantity(quantity);
         displayPrice(calculatePrice(quantity));
@@ -70,14 +88,16 @@ public class MainActivity extends AppCompatActivity {
         CheckBox hasChocolateCheckBox = findViewById(R.id.chocolate_checkbox);
         hasChocolate = hasChocolateCheckBox.isChecked();
         if(hasWhippedTopping==true){
-            total += 10;
+            total += 1;
         }
         if(hasChocolate==true){
-            total += 15;
+            total += 2;
         }
         return total;
     }
     private String createOrderSummary(int quantity){
+        EditText editTextName = findViewById(R.id.editTextName);
+        name = String.valueOf(editTextName.getText());
         String summary = name + "\nQuantity: "+quantity+"\n"
                 + "Add chocolate?"+hasChocolate+"\n"
                 +"Add whipped cream?"+hasWhippedTopping+"\n"
